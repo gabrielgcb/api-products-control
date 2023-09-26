05e96d5350545938ac5817ec9968b9512c686df0
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-09-26 11:39:17 -0300

    feat: ProductRepository

---
+++ src/main/java/com/api/productscontrol/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.api.productscontrol.repositories;
+
+import com.api.productscontrol.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
